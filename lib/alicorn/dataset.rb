24e503f8ae6ecc5a18be6ad0a80c50281a67c485
Ben Somers <somers.ben@gmail.com>
2012-05-15 12:22:26 -0700

    Fixed stats methods for empty datasets

--- lib/alicorn/dataset.rb
+++ lib/alicorn/dataset.rb
@@ -1,14 +1,17 @@
 class DataSet < Array
   def avg
+    return nil if empty?
     inject(:+) / size.to_i
   end
 
   def variance
+    return nil if empty?
     sum=self.inject(0){|acc,i|acc +(i-avg)**2}
     return(1/self.length.to_f*sum)
   end
 
   def stddev
+    return nil if empty?
     Math.sqrt(self.variance)
   end
 end
