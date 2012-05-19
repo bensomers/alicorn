08b74fdf57aecc369dbde74d55faedd2cf886e06
Ben Somers <somers.ben@gmail.com>
2012-05-18 17:43:33 -0700

    Fixed the profiler & parser

--- lib/alicorn/log_parser.rb
+++ lib/alicorn/log_parser.rb
@@ -1,34 +1,24 @@
 module Alicorn
   class LogParser
-    attr_accessor :filename, :samples, :calling, :active, :queued, :calling_avg, :active_avg, :queued_avg
+    attr_accessor :filename, :samples
 
     def initialize(file = "alicorn.log")
       self.filename = file
-      self.samples, self.calling, self.active, self.queued, self.calling_avg, self.active_avg, self.queued_avg = [], [], [], [], [], [], []
+      self.samples  = []
     end
     
     def parse
       f = File.open(filename)
       f.each do |line|
-        if line.match(/Sampling/)
+       if line.match(/Sampling/)
           @sample_hash = {} # this will reset every sample
         elsif line.match(/calling:\[(.+)\]/)
           data = $1.split(", ").map(&:to_i)
-          calling << data
-          @sample_hash[:calling] = data
-        elsif line.match(/calling avg:([\d]+)/)
-          data = $1.to_i
-          calling_avg << data
-          @sample_hash[:calling_avg] = data
+          @sample_hash[:calling] = (DataSet.new << data).flatten
         elsif line.match(/active:\[(.+)\]/)
           data = $1.split(", ").map(&:to_i)
-          active << data
-          @sample_hash[:active] = data
-        elsif line.match(/active avg:([\d]+)/)
-          data = $1.to_i
-          active_avg << data
-          @sample_hash[:active_avg] = data
-          samples << @sample_hash
+          @sample_hash[:active] = (DataSet.new << data).flatten
+          samples << @sample_hash # store the old sample
         end
       end
     end
