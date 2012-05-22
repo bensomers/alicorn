4f60afdf943378fb92180d47308751f216ee7587
Ben Somers <somers.ben@gmail.com>
2012-05-22 13:30:14 -0700

    Wrote mostly-complete test suite for scaler
    Added tests for public interface, covering the 
    private methods for reading the process list. 
    Kept #auto_scale in its own set of tests, because
    it's easier that way. May actually make it public,
    since the profiler directly calls it. Also 
    improved error-handling in several cases, so we
    should safely die anytime we can't quite grok
    what's going on in the process list.

--- lib/alicorn/scaler.rb
+++ lib/alicorn/scaler.rb
@@ -7,17 +7,20 @@ module Alicorn
     attr_accessor :min_workers, :max_workers, :target_ratio, :buffer,
       :raindrops_url, :delay, :sample_count, :app_name, :dry_run, :logger
 
+    attr_reader :signal_delay
+
     def initialize(options = {})
-      self.min_workers        = options[:min_workers]         || 1
-      self.max_workers        = options[:max_workers]
-      self.target_ratio       = options[:target_ratio]        || 1.3
-      self.buffer             = options[:buffer]              || 2
-      self.raindrops_url      = options[:url]                 || "http://127.0.0.1/_raindrops"
-      self.delay              = options[:delay]               || 1
-      self.sample_count       = options[:sample_count]        || 30
-      self.app_name           = options[:app_name]            || "unicorn"
-      self.dry_run            = options[:dry_run]
-      log_path                = options[:log_path]            || "/dev/null"
+      @min_workers        = options[:min_workers]         || 1
+      @max_workers        = options[:max_workers]
+      @target_ratio       = options[:target_ratio]        || 1.3
+      @buffer             = options[:buffer]              || 2
+      @raindrops_url      = options[:url]                 || "http://127.0.0.1/_raindrops"
+      @delay              = options[:delay]               || 1
+      @sample_count       = options[:sample_count]        || 30
+      @app_name           = options[:app_name]            || "unicorn"
+      @dry_run            = options[:dry_run]
+      @signal_delay       = 1
+      log_path            = options[:log_path]            || "/dev/null"
 
       self.logger = Logger.new(log_path)
       logger.level = options[:verbose] ? Logger::DEBUG : Logger::WARN
@@ -27,7 +30,6 @@ module Alicorn
       data          = collect_data
       unicorns      = find_unicorns
 
-      abort "Could not find any unicorn processes" if unicorns.empty?
       master_pid    = find_master_pid(unicorns)
       worker_count  = find_worker_count(unicorns)
 
@@ -35,9 +37,12 @@ module Alicorn
       if sig and !dry_run
         number.times do
           send_signal(master_pid, sig) 
-          sleep(1) # Make sure unicorn doesn't discard repeated signals
+          sleep(signal_delay) # Make sure unicorn doesn't discard repeated signals
         end
       end
+    rescue Exception => e
+      logger.error "exception occurred: #{e.class}\n\n#{e.message}"
+      raise e
     end
 
   protected
@@ -56,8 +61,11 @@ module Alicorn
       target = target.ceil
 
       logger.debug "target calculated at: #{target}, worker count at #{worker_count}"
-      if connections.avg > worker_count and data[:queued].avg > 1
-        logger.debug "danger, will robinson! scaling up fast!"
+      if target >= max_workers
+        logger.warn "at maximum capacity! cannot scale up"
+        return nil, 0
+      elsif connections.avg > worker_count and data[:queued].avg > 1
+        logger.warn "danger, will robinson! scaling up fast!"
         return "TTIN", target - worker_count
       elsif target > worker_count
         logger.debug "scaling up!"
@@ -69,27 +77,6 @@ module Alicorn
         logger.debug "just right!"
         return nil, 0
       end
-    end
-
-    def find_master_pid(unicorns)
-      master_lines = unicorns.select { |line| line.match /master/ }
-      if master_lines.size > 1
-        abort "Too many unicorn master processes detected. You may be restarting, or have an app name collision: #{master_lines}"
-      elsif master_lines.first.match /\(old\)/
-        abort "Old master process detected. You may be restarting: #{master_lines.first}"
-      else
-        master_lines.first.split.first.to_i
-      end
-    end
-
-    def find_worker_count(unicorns)
-      unicorns.select { |line| line.match /worker\[[\d+]\]/ }.count
-    end
-
-    def find_unicorns
-      ptable = %x(ps ax).split("\n")
-      unicorns = ptable.select { |line| line.match(/unicorn/) && line.match(/#{Regexp.escape(app_name)}/) }
-      unicorns.map(&:strip)
     end
 
     def collect_data
@@ -113,15 +100,44 @@ module Alicorn
       {:calling => calling, :writing => writing, :active => active, :queued => queued}
     end
 
+  private
+
+    # Raises errors if the master is busy restarting, or we can't be certain which PID to signal
+    def find_master_pid(unicorns)
+      master_lines = unicorns.select { |line| line.match /master/ }
+      if master_lines.size == 0
+        raise "No unicorn master processes detected. You may still be starting up."
+      elsif master_lines.size > 1
+        raise "Too many unicorn master processes detected. You may be restarting, or have an app name collision: #{master_lines}"
+      elsif master_lines.first.match /\(old\)/
+        raise "Old master process detected. You may be restarting: #{master_lines.first}"
+      else
+        master_lines.first.split.first.to_i
+      end
+    end
+
+    def find_worker_count(unicorns)
+      unicorns.select { |line| line.match /worker\[[\d+]\]/ }.count
+    end
+
+    def find_unicorns
+      ptable = grep_process_list.split("\n")
+      unicorns = ptable.select { |line| line.match(/unicorn/) && line.match(/#{Regexp.escape(app_name)}/) }
+      raise "Could not find any unicorn processes" if unicorns.empty?
+
+      unicorns.map(&:strip)
+    end
+
+    def grep_process_list
+      %x(ps ax)
+    end
+
     def send_signal(master_pid, sig)
       Process.kill(sig, master_pid)
     end
 
-private
-
     def get_raindrops(url)
       Curl::Easy.http_get(url).body_str.split("\n")
     end
-    
   end
 end
--- test/test_scaler.rb
+++ test/test_scaler.rb
@@ -4,13 +4,110 @@ class TestScaler < Test::Unit::TestCase
 
   def setup
     @scaler = Alicorn::Scaler.new(:delay => 0)
+
+    # stub out external call
+    raindrops = "calling: 3\nwriting: 1\n/tmp/cart.socket active: 4\n/tmp/cart.socket queued: 0\n"
+    Curl::Easy.stubs(:http_get).returns(stub(:body_str => raindrops))
+
+    # enable us to test private methods, for the complicated ones
     class << @scaler
-      def publicize(method)
-        self.class.class_eval { public method }
+      def publicize(*methods)
+        methods.each do |meth|
+          self.class.class_eval { public meth }
+        end
       end
     end
   end
 
+  context "#scale!" do
+    context "when no unicorn processes are running" do
+      setup do
+        @scaler.stubs(:grep_process_list).returns("foo\nbar")
+      end
+
+      should "raise an error" do
+        exception = assert_raise(RuntimeError) do
+          @scaler.scale!
+        end
+        assert_equal "Could not find any unicorn processes", exception.message
+      end
+    end
+
+    context "when no master processes are running" do
+      setup do
+        plist = "1050 ?  Sl  1:06 unicorn_rails worker[0] -c first_unicorn.rb -E staging -D\n 
+                 1051 ?  Sl  1:06 unicorn_rails worker[1] -c other_unicorn.rb -E staging -D\n"
+        @scaler.stubs(:grep_process_list).returns(plist)
+      end
+
+      should "raise an error" do
+        exception = assert_raise(RuntimeError) do
+          @scaler.scale!
+        end
+        assert_match /No unicorn master processes/, exception.message
+      end
+    end
+
+    context "when multiple master processes are found" do
+      setup do
+        plist = "1050 ?  Sl  1:06 unicorn_rails master -c first_unicorn.rb -E staging -D\n 
+                 1051 ?  Sl  1:06 unicorn_rails master -c other_unicorn.rb -E staging -D\n"
+        @scaler.stubs(:grep_process_list).returns(plist)
+      end
+
+      should "raise an error" do
+        exception = assert_raise(RuntimeError) do
+          @scaler.scale!
+        end
+        assert_match /Too many unicorn master processes/, exception.message
+      end
+    end
+
+    context "when an old master process is running" do
+      setup do
+        plist = "1050 ?  Sl  1:06 unicorn_rails master (old) -c first_unicorn.rb -E staging -D\n 
+                 1051 ?  Sl  1:06 unicorn_rails worker[0] -c other_unicorn.rb -E staging -D\n"
+        @scaler.stubs(:grep_process_list).returns(plist)
+      end
+
+      should "raise an error" do
+        exception = assert_raise(RuntimeError) do
+          @scaler.scale!
+        end
+        assert_match /Old master process detected/, exception.message
+      end
+    end
+
+    context "when things are properly detected" do
+      setup do
+        plist = "1050 ?  Sl  1:06 unicorn_rails master -c first_unicorn.rb -E staging -D\n 
+                 1051 ?  Sl  1:06 unicorn_rails master -c other_unicorn.rb -E staging -D\n
+                 1052 ?  Sl  1:06 unicorn_rails worker[0] -c other_unicorn.rb -E staging -D\n
+                 1053 ?  Sl  1:06 unicorn_rails worker[1] -c other_unicorn.rb -E staging -D\n
+                 1054 ?  Sl  1:06 unicorn_rails worker[0] -c first_unicorn.rb -E staging -D\n" 
+        @scaler.stubs(:grep_process_list).returns(plist)
+        @scaler.stubs(:auto_scale).returns(["NOTASIGNAL", 2])
+        @scaler.stubs(:signal_delay).returns(0)
+        @scaler.app_name = "first"
+      end
+
+      should "find the correct master and correct worker quantity" do
+        @scaler.publicize(:find_unicorns, :find_master_pid, :find_worker_count)
+        unicorns = @scaler.find_unicorns
+
+        assert_equal 1050, @scaler.find_master_pid(unicorns)
+        assert_equal 1, @scaler.find_worker_count(unicorns)
+      end
+
+      should "send the correct number of signals" do
+        @scaler.expects(:send_signal).with(1050, "NOTASIGNAL").twice
+        @scaler.scale!
+        assert true
+      end      
+    end
+  end
+
+  # Test the scaling algorithm separately for simplicity's sake
   context "#auto_scale" do
     setup do
       @scaler.publicize :auto_scale
@@ -24,7 +121,7 @@ class TestScaler < Test::Unit::TestCase
       @data[:queued]
     end
 
-    context "when we're above the target" do
+    context "when we need to scale down" do
       setup do
         @data[:active] << 3 << 4 << 5
         @data[:queued] << 0 << 0 << 0
@@ -33,9 +130,15 @@ class TestScaler < Test::Unit::TestCase
       should "return 1 TTOU" do
         assert_equal ["TTOU", 1], @scaler.auto_scale(@data, @worker_count)
       end
+
+      context "but we've hit our minimum workers" do
+        should "return no signal" do
+          flunk
+        end
+      end
     end
 
-    context "when we're below the target" do
+    context "when we need to scale up" do
       setup do
         @data[:active] << 2 << 10
         @data[:queued] << 0 << 0
@@ -44,9 +147,19 @@ class TestScaler < Test::Unit::TestCase
       should "return 1 TTIN" do
         assert_equal ["TTIN", 1], @scaler.auto_scale(@data, @worker_count)
       end
+
+      context "but we've hit our maximum workers" do
+        setup do
+          @scaler.max_workers = 10
+        end
+
+        should "return no signal" do
+          assert_equal [nil, 0], @scaler.auto_scale(@data, @worker_count)
+        end
+      end
     end
 
-    context "when we need to scale up fast" do
+    context "when we need to panic and scale up fast" do
       setup do
         @data[:queued] << 6
         @data[:active] << 6
@@ -63,7 +176,7 @@ class TestScaler < Test::Unit::TestCase
         @data[:queued] << 0
       end
 
-      should "return several TTIN" do
+      should "return no signal" do
         assert_equal [nil, 0], @scaler.auto_scale(@data, @worker_count)
       end
     end
@@ -73,8 +186,6 @@ class TestScaler < Test::Unit::TestCase
   context "#collect_data" do
     setup do
       @scaler.publicize :collect_data
-      raindrops = "calling: 3\nwriting: 1\n/tmp/cart.socket active: 4\n/tmp/cart.socket queued: 0\n"
-      Curl::Easy.stubs(:http_get).returns(stub(:body_str => raindrops))
     end
 
     should "return the correct data" do
